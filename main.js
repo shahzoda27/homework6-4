5e91e765b650a966a9b585165c926f9921c7d8f7
. <.>
2023-10-19 17:45:49 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//1-masala
+/*let array = ['Shahzoda','Ali','Yasmina','Amirbek','Sadiya','Mustafo']
+let user1 = prompt('Userni kirit')
+let  user = array.indexOf(user1)
+console.log(`user= ${user}`);*/
+
+
+//2-masala
+/*let arr1 = [28,5,6,8,22,88]
+let arr2 = [arr1[0]]
+let  arr2 = Math.min(...arr1)
+let  arr3 = Math.max(...arr1)
+console.log(arr2);
+console.log(arr3);
+*/
+//3-masala
+let array1 = [1,2,3,4,5,6,7]
+let array2 = []
+for(i = 0; i < array1.length; i++){
+    if(array1[i]< 3){
+        console.log(array1[i]);
+    }
+}
+console.log(array2);
